487dd9dea0a9155aa4419d08a4919886ed45cc24
stwater20 <50062014+stwater20@users.noreply.github.com>
2021-04-08 21:57:42 +0800

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,42 @@
+<?php
+
+
+require("database.inc.php");
+$link = mysqli_connect($SERVER, $USERNAME, $PASSWORD) or die("Connect Failed");
+mysqli_query($link, "SET NAMES 'UTF8'");
+mysqli_select_db($link, $DATABASE) or die("Select Failed");
+
+// define variables and set to empty values
+$user_id = $email = $picture_url = "";
+
+$allowed_hosts = array('ntutfaq.herokuapp.com');
+if (!isset($_SERVER['HTTP_HOST']) || !in_array($_SERVER['HTTP_HOST'], $allowed_hosts)) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+} else {
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_id = test_input($_POST["user_id"]);
+    $display_name = test_input($_POST["display_name"]);
+    $picture_url = test_input($_POST["picture_url"]);
+    $query_show = "SELECT * FROM LINE_profiles WHERE user_id = '$user_id'";
+    $result_show = mysqli_query($link, $query_show) or die();
+    if (mysqli_num_rows($result_show) == 0) {
+      $sql = "INSERT INTO LINE_profiles VALUES('$user_id','$display_name','$picture_url',0)";
+      $result = mysqli_query($link, $sql) or die();
+    } else {
+      $target_row = mysqli_fetch_array($result_show);
+      $count = $target_row[3] + 1;
+      $sql = "UPDATE LINE_profiles SET count = $count WHERE user_id = '$user_id'";
+      $result = mysqli_query($link, $sql) or die();
+    }
+  }
+}
+
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+echo $user_id . " " . $display_name . " " . $picture_url;
